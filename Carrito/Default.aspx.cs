be61fcbbdc13e14f3b9d933174ae1d0f73a2f7f8
Analia <ana.lau.villarreal@gmail.com>
2022-06-05 15:27:40 -0300

    primer commit

---
+++ Carrito/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+using negocio;
+
+namespace Carrito
+{
+    public partial class _Default : Page
+    {
+
+        
+        public List<Articulo> listaArt { get; set;}    
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+
+            listaArt = negocio.Listar();       
+
+    }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+}
